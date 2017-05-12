7629d353c6cc6fcc3775bf4e1a6c5f4223ca8ea7
Eetu Pajuoja <eetu.pajuoja@solita.fi>
2017-05-12 13:24:55 +0300

    Training day done.

--- test/training_day_test.clj
+++ test/training_day_test.clj
@@ -1,2 +1,8 @@
+;;(ns training-day-test
+  ;;(:use iloveponies.tests.training-day))
+
 (ns training-day-test
-  (:use iloveponies.tests.training-day))
+  (:require [training-day :refer :all]))
+
+
+(require 'iloveponies.tests.training-day :reload)
